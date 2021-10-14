9cf37fc692f97a673cad254f1bd0affc680e862f
littlepig <1193639712@qq.com>
2021-10-14 10:47:47 +0800

    feat: 初始化替换文本为key的插件

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const replace = require('../src/replace')
+
+// 在命令行先执行export REPLACE_ENV = true
+// 再运行webpack文件，否则获取REPLACE_ENV的默认值
+class Replace {
+    constructor(options) {
+        this.options = options
+    }
+    apply(compiler) {
+        compiler.hooks.done.tap('replace', (compile)=> {
+            replace(this.options.langRerource, this.options.targetDir||'')
+        })
+    }
+}
+
+module.exports = Replace
